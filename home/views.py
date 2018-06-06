bb1ce44c83bfcf31ef1806e5e37bc6bded4dff6a
MarcPlaying <herpersmarcandre@gmail.com>
2018-06-06 11:02:51 +0200

    deleted notfound site

--- home/views.py
+++ home/views.py
@@ -6,5 +6,3 @@ def server(request):
         return render(request, 'home/servers.html')
 def index(request):
         return render(request, 'home/home.html')
-def nf(request):
-        return render(request, 'home/nf.html')
