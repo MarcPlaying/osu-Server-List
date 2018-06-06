e001d1614685b3d7cee1b307cdbdbbc6b5b7e2ca
MarcPlaying <herpersmarcandre@gmail.com>
2018-06-06 11:23:25 +0200

    added the new add function

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
+    url(r'^add/server', views.addserver, name='addserver'),
+    url(r'^add/$', views.add, name='add'),
     url(r'^servers/$', views.server, name='servers'),
     url(r'^home/$', views.index, name='index'),
     url(r'^$', views.index, name='index'),
--- home/views.py
+++ home/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import MySQLdb
 
+def addserver(request):
+        return render(request, 'home/addserver.html')
+def add(request):
+        return render(request, 'home/add.html')
 def server(request):
         return render(request, 'home/servers.html')
 def index(request):
