74455952f195448e39741ce60f4eac41b5f234f8
MarcPlaying <herpersmarcandre@gmail.com>
2018-06-07 13:19:31 +0200

    Fixed the unlimeted ref

--- home/views.py
+++ home/views.py
@@ -3,11 +3,15 @@ from django.http import HttpResponse
 import MySQLdb
 
 def ref(request):
-        query = request.GET.get('s')
+    query = request.GET.get('s')
+    if query in "list.enjuu.click":
         context= {
             'server': query,
         }
         return render(request, 'home/redirect.html', context)
+    else:
+        return render(request, 'home/redirect.html')
+
 def addserver(request):
         return render(request, 'home/addserver.html')
 def add(request):
