22e96bb99e40ed862adda1e4ec8c226c4cda7740
KazukiDEV <herpersmarcandre@gmail.com>
2018-06-08 21:35:43 +0200

    fixed some URLs lol

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^ref', views.ref, name='ref'),
-    url(r'^add/server', views.addserver, name='addserver'),
+    url(r'^ref$', views.ref, name='ref'),
+    url(r'^add/server$', views.addserver, name='addserver'),
     url(r'^add/$', views.add, name='add'),
     url(r'^servers/$', views.server, name='servers'),
     url(r'^home/$', views.index, name='index'),
