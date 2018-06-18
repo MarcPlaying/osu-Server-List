3a9a34e8514857bd898af3e4054a2f8599c801c3
MarcPlaying <herpersmarcandre@gmail.com>
2018-06-18 18:09:06 +0200

    Add a page to view more about the Server

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
+    url(r'^view$', views.viewserver, name='vs'),
     url(r'^ref$', views.ref, name='ref'),
     url(r'^add/server$', views.addserver, name='addserver'),
     url(r'^add/$', views.add, name='add'),
--- home/views.py
+++ home/views.py
@@ -1,6 +1,94 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import MySQLdb
+
+def viewserver(request):
+    query = request.GET.get('s')
+    if(query == '1'):
+        context= {
+            'web': '/ref?s=akatsuki.pw',
+            'img': 'https://marc.enjuu.click/download/1930773.png',
+            'servername': 'Akatsuki',
+            'smalldes': 'STD & RX PP',
+            'longdes': 'Akatsuki is Features Relax and STD PP',
+            'players': '1679',
+        }
+        return render(request, 'home/nodes.html', context)
+    elif(query == '2'):
+        context= {
+            'web': '/ref?s=enjuu.click',
+            'img': 'https://enjuu.click/static/logos/logo-blue.png',
+            'servername': 'Enjuu',
+            'smalldes': 'STD, AP, RX PP',
+            'longdes': 'Enjuu is a osu! Private Server that exist since January. Featuring RX and many more Features. ',
+            'players': '504',
+            'rlylongdes': 'This is the Server that runs this Server List. Our private osu! Server. It supports nearly all Gamemodes so STD, AP, RX but nerfed',
+        }
+        return render(request, 'home/view.html', context)
+    elif(query == '3'):
+        context= {
+            'web': '/ref?s=ripple.moe',
+            'img': 'https://marc.enjuu.click/download/1482369.png',
+            'servername': 'Ripple',
+            'smalldes': 'STD PP',
+            'longdes': 'Ripple is the Private Server with the most Users. ',
+            'players': '62381 ',
+        }
+        return render(request, 'home/nodes.html', context)
+    elif(query == '4'):
+        context= {
+            'web': '/ref?s=osu.gatari.pw/?e',
+            'img': 'https://marc.enjuu.click/download/9381005.png',
+            'servername': 'Gatari',
+            'smalldes': 'STD PP',
+            'longdes': 'Gatari is a Russian osu Server. With a nice Clan System ',
+            'players': '6778 ',
+        }
+        return render(request, 'home/nodes.html', context)
+    elif(query == '5'):
+        context= {
+            'web': '/ref?s=vipsu.ml',
+            'img': 'https://marc.enjuu.click/download/2325559.png',
+            'servername': 'Vipsu',
+            'smalldes': 'STD own weightend PP',
+            'longdes': 'Vipsu is a small and new osu! Private Server ',
+            'players': '19 ',
+        }
+        return render(request, 'home/nodes.html', context)
+    elif(query == '6'):
+        context= {
+            'web': '/ref?s=skycircles.ru',
+            'img': 'https://marc.enjuu.click/download/3832546.png',
+            'servername': 'SkyCircles',
+            'smalldes': 'STD PP',
+            'longdes': 'SkyCircles is a new Russian osu Server ',
+            'players': '91',
+        }
+        return render(request, 'home/nodes.html', context)
+    elif(query == '7'):
+        context= {
+            'web': '/ref?s=gigamons.de',
+            'img': 'https://marc.enjuu.click/download/6312014.png',
+            'servername': 'Gigamons',
+            'smalldes': 'RX&TD PP',
+            'longdes': 'Seperate RX Scoreboard and leaderboard. Seperate TD Scoreboard and leaderboard. Trollerino. All default bancho stuff, such as beatmap submittion. A few comming soon. (written by owner) ',
+            'players': 'NONE',
+        }
+        return render(request, 'home/nodes.html', context)
+    elif(query == '8'):
+            context= {
+                'web': '/ref?s=osu.xii.nz',
+                'img': 'https://marc.enjuu.click/download/4484948.png',
+                'servername': 'XII',
+                'smalldes': 'STD PP',
+                'longdes': 'XII is a small and new osu Server ',
+                'players': '20',
+            }
+            return render(request, 'home/nodes.html', context)
+    else:
+        return render(request, 'home/nf.html')
+
+
+
 
 def ref(request):
     query = request.GET.get('s')
